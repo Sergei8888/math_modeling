79dad2bc0db4bc0d511cf3fc74ae00c8fc1cc92a
Sergei8888 <45688549+Sergei8888@users.noreply.github.com>
2020-10-15 17:44:56 +0200

    Update lab3_ex2.py

--- lab3_ex2.py
+++ lab3_ex2.py
@@ -4,4 +4,6 @@ import math
 
 A = constants.g*100*(np.tan(np.radians(30))**2)
 B = 2 * (np.cos(math.pi/3)**2)*(1-np.tan(np.radians(30))*np.tan(math.pi/3))
+N = (2/np.sqrt(numpy.pi)) * (constants.h/(constants.k*200)**3/2) * constants.e**(-(constants.e)/((constants.k)*200))
 print(np.sqrt(A/B))
+print(N)
