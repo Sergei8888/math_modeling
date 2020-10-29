1b29839a3c37d642c3f4b63554091070c57e9684
Sergei8888 <45688549+Sergei8888@users.noreply.github.com>
2020-10-29 17:38:43 +0200

    ex
    ex

---
+++ lab5_ex4.py
@@ -0,0 +1,12 @@
+import numpy as np
+import random as rnd
+
+def znachenia(a, b, x, n):
+    mass = []
+    ranage_of_zhachenii = np.linspace(a, b, n)
+    
+    for i in ranage_of_zhachenii:
+        mass.append(i**2)
+    return range_of_znachenii
+
+
