d9d36e1e860874dd9774248d583981470f8a0c73
Sergei8888 <45688549+Sergei8888@users.noreply.github.com>
2020-11-05 16:24:54 +0200

    ex
    ex

--- lab6_ex1.py
+++ lab6_ex1.py
@@ -1,8 +1,9 @@
 def mult(a=0, n=0):
     a = int(input())
     n = int(input())
-    for i in range(n-1):
-        a *= a
-    return a
+    x = 1
+    for i in range(n):
+        x = x * a
+    return x
 print(mult())
 
